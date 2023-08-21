2a75cdea8250208480584d7455f17ddd9412b5d1
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-08-21 17:53:14 +0530

    completed profile page with firestore

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,10 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:petking/auth/email/login_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String email = "";
+  String fullName = "";
+  String profilePic = '';
+  String username = '';
+  Future fetchData() async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      final uid = FirebaseAuth.instance.currentUser!.uid;
+      final res =
+          await FirebaseFirestore.instance.collection("users").doc(uid).get();
+      final data = res.data();
+      email = data!["email"] ?? "";
+      fullName = data["full_name"] ?? "";
+      profilePic = data["profilepic"] ?? "";
+      username = data["username"] ?? "";
+      setState(() {});
+    }
+  }
+
+  void logOut() {
+    FirebaseAuth.instance.signOut();
+    Get.off(const LoginPage());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                logOut();
+              },
+              icon: Icon(Icons.logout))
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FutureBuilder(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Email: $email"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text("Full Name: $fullName"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text("Profile Pic: $profilePic"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text("Username: $username"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                );
+              })
+        ],
+      ),
+    );
   }
 }
---
+++ lib/view/profile_view.dart
@@ -0,0 +1,205 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:petking/view/home_view.dart';
+
+// ignore: must_be_immutable
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController userFullNameController = TextEditingController();
+  TextEditingController userNameController = TextEditingController();
+
+  File? pickedFile;
+
+  void selectProfile() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.image);
+    if (result != null) {
+      pickedFile = File(result.files.single.path!);
+      setState(() {});
+    }
+  }
+
+  Future<String?> uploadPic() async {
+    if (pickedFile == null) {
+      return null;
+    }
+    const path = "files/img.png";
+    final file = File(pickedFile!.path);
+    final actualFireStrorage = FirebaseStorage.instance.ref().child(path);
+    UploadTask uploadTask = actualFireStrorage.putFile(file);
+
+    final snapshot = await uploadTask.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    return urlDownload;
+  }
+
+  void createProfile(String profilePicLink) async {
+    String userFullName = userFullNameController.text.trim();
+    String userName = userNameController.text.trim();
+
+    if (userName == "" || userFullName == "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("All Information are required"),
+        ),
+      );
+    } else {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        return;
+      }
+      await FirebaseFirestore.instance.collection("users").doc(user.uid).set({
+        "email": user.email,
+        "full_name": userFullName,
+        "username": userName,
+        "profilepic": profilePicLink,
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          const Center(
+            child: Text(
+              "Create petking profile",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          InkWell(
+            onTap: () {
+              selectProfile();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxHeight: 100,
+                    maxWidth: 100,
+                  ),
+                  child: pickedFile != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.file(
+                            pickedFile!,
+                            fit: BoxFit.cover,
+                            width: 150,
+                            height: 150,
+                          ),
+                        )
+                      : Container(
+                          height: 150,
+                          width: 150,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(255, 228, 162, 157),
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+          const Center(
+              child: Text(
+            "Select profile pic",
+            style: TextStyle(color: Colors.blue),
+          )),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text("full name"),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.06,
+            child: TextField(
+              controller: userFullNameController,
+              decoration: const InputDecoration(
+                hintText: " full name",
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 11, 12, 11)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("username"),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.06,
+            child: TextField(
+              controller: userNameController,
+              decoration: const InputDecoration(
+                hintText: "username",
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 11, 12, 11)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          const Expanded(child: SizedBox()),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 228, 162, 157),
+                minimumSize: const Size(400, 50),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            onPressed: () async {
+              final profilepic = await uploadPic();
+              if (profilepic != null) {
+                createProfile(profilepic);
+                Get.offAll(() => const HomePage());
+              }
+            },
+            child: const Text(
+              'Create profile ',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          )
+        ]),
+      ),
+    );
+  }
+}
